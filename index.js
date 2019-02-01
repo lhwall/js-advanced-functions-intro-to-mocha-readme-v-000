90fe974a9f3d931750a9fcfb5e5f0b8ad1ba0c7f
Lucy Wall <lhwall@gmail.com>
2019-02-01 18:11:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(icecream){
+  return `I love ${icecream}`
+}
+
+function shouting(entry){
+  return entry.toUpperCase()
+}
+
+function roundDown(number){
+  return Math.floor(parseInt(number))
+}
